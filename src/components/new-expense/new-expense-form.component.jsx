f3591736e714dc0473c606408508cd519d71630d
servosdesign <servos.design0@gmail.com>
2022-03-15 18:10:16 -0500

    form submission link, single state two way bind

--- src/components/new-expense/new-expense-form.component.jsx
+++ src/components/new-expense/new-expense-form.component.jsx
@@ -43,8 +43,21 @@ const ExpenseForm = () => {
     });
   };
 
-  const submitHandler = () => {
+  const submitHandler = event => {
+    event.preventDefault();
+    
+    const expenseData = {
+      title: userInput.enteredTitle,
+      amount: userInput.enteredAmount,
+      date: new Date(userInput.enteredDate + 'T00:00:00')
+    };
 
+    console.log(expenseData);
+    setUserInput({
+    enteredTitle : '' , 
+    enteredAmount : '',
+    enteredDate : ''
+    });
   };
 
   return (
@@ -52,25 +65,16 @@ const ExpenseForm = () => {
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' onChange={titleChangeHandler} />
+          <input type='text' value={userInput.enteredTitle} onChange={titleChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input
-            type='number'
-            min='0.01'
-            step='0.01'
-            onChange={amountChangeHandler}
-          />
+          <input type='number' value={userInput.enteredAmount} min='0.01' step='0.01' onChange={amountChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
           <input
-            type='date'
-            min='2019-01-01'
-            max='2022-12-31'
-            onChange={dateChangeHandler}
-          />
+            type='date' value={userInput.enteredDate} min='2019-01-01' max='2022-12-31' onChange={dateChangeHandler} />
         </div>
       </div>
       <div className='new-expense__actions'>
