e0876ce375038d2e97890890f702fb0c8d66b60f
servosdesign <servos.design0@gmail.com>
2022-03-20 14:34:50 -0500

    entered values integration for new-expense-form component

--- src/components/expense-item/expense-item.component.jsx
+++ src/components/expense-item/expense-item.component.jsx
@@ -1,24 +1,17 @@
-import { useState } from 'react';
 import ExpenseDate from '../expense-date/expense-date.component';
 import Card from '../card/card.component';
 
 import './expense-item.styles.css';
 
 const ExpenseItem = props => {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = (props) => {
-    setTitle('Updated!');
-  };
-
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className='expense-item__price'>{props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
+      
     </Card>
   );
 };
